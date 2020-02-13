2a3063bd9e7f356d0006a42fac9fa69630f08815
KNeumann8 <kneumann8@my.wctc.edu>
2020-02-12 23:22:04 -0600

    done

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -43,10 +43,77 @@ public class Main {
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
+
+        //My stuff:
+        Map.Entry<String,Integer>[] watchMojo = new Map.Entry[10]; //<String,Integer> is required for it expect proper value and key types it seems
+
+
+            for(Map.Entry<String , Integer> e : map.entrySet()){
+                for(int i = 9; i>=0; i--){
+                    if((watchMojo[i] == null)){
+                        watchMojo[i] = e;
+                    }
+                    else if((watchMojo[i].getValue() < e.getValue())){
+                        if(i>0&&!(watchMojo[i-1].getValue() <= e.getValue()||watchMojo[i-1] == null)){
+                            if(i<9){
+                                Map.Entry<String,Integer> bob = watchMojo[i];
+                                Map.Entry<String,Integer> fred = watchMojo[i];
+                                watchMojo[i] = e;
+                                for(int counting = i+1; counting<=9;counting++){
+                                    fred = watchMojo[counting];
+                                    watchMojo[counting] = bob;
+                                    bob = fred;
+                                }
+                            }
+                            else watchMojo[i] = e;
+                        }
+                        else if(i==0){
+
+                            if(i<9){
+                                Map.Entry<String,Integer> bob = watchMojo[i];
+                                Map.Entry<String,Integer> fred = watchMojo[i];
+                                watchMojo[i] = e;
+                                for(int counting = i+1; counting<=9;counting++){
+                                    fred = watchMojo[counting];
+                                    watchMojo[counting] = bob;
+                                    bob = fred;
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+        System.out.println("Top Ten Most Written Words:");
+        int counting = 0;
+        for(Map.Entry<String,Integer> e : watchMojo){
+            System.out.println(++counting +": "+e.getKey() + " is used " + e.getValue()+" times");
+        }
+
+        System.out.println("Words used once:");
+        int h = 10;
+        for(Map.Entry<String , Integer> e : map.entrySet()){
+            if(e.getValue()==1){
+                h--;
+                System.out.print(e.getKey());
+                if(h==0){
+                    System.out.println();
+                    h = 10;
+                }
+                else System.out.print(", ");
+            }
+        }
+
     }
 
     public static void main(String[] args) {
         new Main();
     }
+
+
 
 }
